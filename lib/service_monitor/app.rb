c205e37f1d246948942a7e14fc122a822b3096e5
Jude Quintana <jude.quintana@gmail.com>
2016-08-29 13:24:29 -0600

    moved loading yaml to method

--- lib/service_monitor/app.rb
+++ lib/service_monitor/app.rb
@@ -1,6 +1,5 @@
 module ServiceMonitor
   class App < Sinatra::Base
-    set :service_config, YAML.load_file(File.expand_path('../../../config/service_config.yaml', __FILE__))
 
     get '/' do
       @services = get_status_for_services
@@ -10,6 +9,10 @@ module ServiceMonitor
 
 
     private
+
+    def load_config
+      YAML.load_file(File.expand_path('../../../config/service_config.yaml', __FILE__))
+    end
 
     def build_service_objects
       settings.service_config.map { |config|
